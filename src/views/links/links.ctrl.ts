98a3f4505eb5a30204bc067a85aec7dc2968262a
Steve Beaudry <lordsteve.tga@gmail.com>
2026-05-25 16:17:25 -0400

    add title to links page

--- src/views/links/links.ctrl.ts
+++ src/views/links/links.ctrl.ts
@@ -1,5 +1,6 @@
 import el from '@elements'
 
 export default function links() {
-    if (el.nav) el.nav.style.display = 'none';
+    if (el.title) el.title.innerText = "Lord Steve's Links";
+    if (el.nav) el.nav.style.display = "none";
 }
