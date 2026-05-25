1252c4a5198efcb09504b34850b13834d54a8196
Steve Beaudry <lordsteve.tga@gmail.com>
2026-05-25 10:40:04 -0400

    removing unnecessary helper to clean up css

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -1,16 +1,3 @@
 export default class Helpers {
-    static centerSection(section: HTMLElement) {
-        // this is to compensate for some weird css flex behavior
-        const parent = section.parentElement;
-        if (!parent) return;
-        const rect = section.getBoundingClientRect();
-        const parentRect = parent.getBoundingClientRect();
-        const distance = rect.bottom - parentRect.bottom;
-        const screenWidth = window.innerWidth;
-        if (distance < 0 && screenWidth > 768) {
-            section.classList.remove('flex-align-center');
-        } else {
-            section.classList.add('flex-align-center');
-        }
-    }
+    // Place global helper functions here as needed
 }
--- src/views/about/about.ctrl.ts
+++ src/views/about/about.ctrl.ts
@@ -1,12 +1,5 @@
 import el from '@elements';
-import Helpers from '@services/helpers';
 
 export default function about() {
     el.title.textContent = 'About The Green Asterisk';
-    
-    const section = el.sections ? el.sections[0] : null;
-    if (section) {
-        Helpers.centerSection(section);
-        window.addEventListener('resize', () => Helpers.centerSection(section));
-    }
 }
