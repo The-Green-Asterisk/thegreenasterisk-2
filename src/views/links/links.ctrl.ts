1293e993346186c21e8866151c3bab84fd2b61ea
Steve Beaudry <lordsteve.tga@gmail.com>
2026-08-15 13:25:13 -0400

    Implements link creation flow in client-side view
    Adds logic to trigger the link creation modal and includes necessary
    CSS for modal presentation.

--- src/views/links/links.ctrl.ts
+++ src/views/links/links.ctrl.ts
@@ -1,6 +1,45 @@
 import el from '@elements';
+import Helpers from '@services/helpers';
+import { getHtml } from '@services/request';
+import Link from '../../entities/Link';
+import nav from '@views/nav/nav.ctrl';
+const html = Helpers.html;
 
 export default function links() {
     if (el.title) el.title.innerText = "Lord Steve's Links";
-    if (el.nav) el.nav.style.display = "none";
+    nav(false); //take away nav
+
+    const addLinkModel: Link = {
+        url: "#",
+        primaryType: true,
+        imageUrl: "/storage/images/link.png",
+        iconClass: "",
+        text: "Create New Link"
+    }
+
+    const buildLink = (linkModel: Link, stay: boolean = false) => html`
+        <a class="link" target="${stay ? '_self' : '_blank'}" href="${linkModel.url}">
+            <div class="link-image ${linkModel.primaryType ? 'primary' : ''}">
+                ${linkModel.imageUrl
+            ? `<img src="${linkModel.imageUrl}" alt="${linkModel.text}">`
+            : `<span class="fab ${linkModel.iconClass}"></span>`
+        }
+            </div>
+            <div class="link-text">
+                ${linkModel.text}
+            </div>
+        </a>
+    `;
+
+    const newLinkButton = buildLink(addLinkModel, true);
+    newLinkButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        getHtml<HTMLElement>('data/create-link').then(response => {
+            el.body.appendChild(response);
+        });
+    });
+    const linksSection = el.links?.querySelector('section');
+    if (linksSection) {
+        linksSection.appendChild(newLinkButton);
+    }
 }
